0acf5d51bdf4da05e5f2291f99c567a21bb81c36
Mitch Hartigan <mitch.hartigan@gmail.com>
2021-12-03 12:55:09 -0800

    feat: connected to v3 acronyms

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const _loadCollection = async () => {
   try {
     const client = new MongoClient(dbUrl);
     await client.connect();
-    let collection = client.db("mortgagebanking").collection("UpdatedAcronyms");
+    let collection = client.db("mortgagebanking").collection("acronymsv3");
 
     return collection;
   } catch (err) {
